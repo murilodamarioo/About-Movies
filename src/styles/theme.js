beaacef0face446c4c85ac5d698b3d9c9a8bab90
Murilo Damario <murilo.gcdamario@gmail.com>
2024-05-13 23:02:17 -0300

    Create theme colors file and global styles file

---
+++ src/styles/theme.js
@@ -0,0 +1,29 @@
+export default {
+    COLORS: {
+        //Buttons color
+        PINK: '#FF859B',
+        
+        // Text color
+        WHITE_400: '#FFFFFF',
+        WHITE_300: '#F4EDE8',
+        WHITE_200: 'C4C4CF',
+        WHITE_100: 'E5E5E5',
+
+        // Placeholder color
+        GRAY_100: '#048F99',
+        
+        // Input color
+        GRAY_200: '#262529',
+
+        // Main Background color
+        BACKGROUND_100: '#1C1B1E',
+        // Header Background color
+        BACKGROUND_200: '#3E3B47',
+
+        // Button text color
+        BLACK_200: '#000000',
+
+        // Second Button color and Backgroud
+        BLACK_100: '#0D0C0F'
+    }
+}
