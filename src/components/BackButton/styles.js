e206152ca6a24c1b13861bc05e08c378fb872d82
Murilo Damario <murilo.gcdamario@gmail.com>
2024-05-15 23:06:00 -0300

    Create BackButton component

---
+++ src/components/BackButton/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.a`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .8rem;
+    margin-top: 4.2rem;
+
+    color: ${({ theme }) => theme.COLORS.PINK};
+`
