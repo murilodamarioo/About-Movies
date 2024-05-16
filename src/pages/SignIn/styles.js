c7b7d8e05ff5af8757dfa40949a0ba96dc9de80e
Murilo Damario <murilo.gcdamario@gmail.com>
2024-05-15 23:08:11 -0300

    Add align-self in h2 css

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -30,6 +30,7 @@ export const Form = styled.form`
     > h2 {
         font-size: 2.4rem;
         font-weight: 500;
+        align-self: flex-start;
         margin: 4.8rem 0;
         color: ${({ theme }) => theme.COLORS.WHITE_300};
     }
