7f373104754a1c22260ea834e803a71eab65b1ce
Murilo Damario <murilo.gcdamario@gmail.com>
2024-05-18 17:33:01 -0300

    Add margin-right

--- src/components/Tag/styles.js
+++ src/components/Tag/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.span`
     font-size: 1.2rem;
     padding: 0.8rem 1.6rem;
-    margin: .8rem;
+    margin-right: .8rem;
     border-radius: 0.8rem;
 
     color: ${({ theme }) => theme.COLORS.WHITE_100};
