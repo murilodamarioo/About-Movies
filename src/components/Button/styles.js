dcd06fce42e38ce98b02133cb8366604104122c8
Murilo Damario <murilo.gcdamario@gmail.com>
2024-05-19 22:51:06 -0300

    Add icon on Button component

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,6 +4,10 @@ export const Container = styled.button`
     width: 100%;
     height: 5.6rem;
 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
     background-color: ${({ theme }) => theme.COLORS.PINK};
     color : ${({ theme }) => theme.COLORS.BLACK_200};
 
@@ -13,4 +17,8 @@ export const Container = styled.button`
     padding: 0 1.6rem;
     font-weight: 500;
     margin-top: 1.6rem;
+
+    > svg {
+        margin-right: .8rem;
+    }
 `
