9bb16389e4708e2295a2dbee7d928f3e6f1241c5
Murilo Damario <murilo.gcdamario@gmail.com>
2024-07-15 22:24:43 -0300

    Implement user Sign Up feature

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,7 +6,35 @@ import { BackButton } from '../../components/BackButton'
 
 import { Container, Form, Background } from './styles'
 
+import { useState } from 'react'
+import { api } from '../../services/api'
+import { useNavigate } from 'react-router-dom'
+
 export function SignUp() {
+    const navigate = useNavigate()
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSignUp() {
+        if (!name || !email || !password) {
+            return alert('Preencha todos os dados')
+        }
+
+        api.post('/users', { name, email, password })
+            .then(() => {
+                alert('Usuário Cadastrado com sucesso')
+                navigate('/')
+            })
+            .catch(error => {
+                if (error.message) {
+                    alert(error.response.data.message)
+                } else {
+                    alert('Não foi possível cadastrar o usuário!')
+                }
+            })
+    }
     
     return (
         <Container>
@@ -20,21 +48,24 @@ export function SignUp() {
                     icon={FiUser}
                     type="text"
                     placeholder="Nome"
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input
                     icon={FiMail}
                     type="text"
                     placeholder="E-mail"
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     icon={FiLock}
                     type="password"
                     placeholder="Senha"
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Cadastrar"/>
+                <Button title="Cadastrar" onClick={handleSignUp} />
 
                 <BackButton to="/" icon={FiArrowLeft} title="Voltar para o login"/>
             </Form>
