969cdedb1d5af34b90a1402642b7971d1ffe4a34
Murilo Damario <murilo.gcdamario@gmail.com>
2024-07-23 23:27:29 -0300

    implement user update

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -33,6 +33,25 @@ function AuthProvider({ children }) {
     setData({})
   }
 
+
+  async function updateProfile({user}) {
+
+    try {
+      await api.put('/users', user)
+      localStorage.setItem('@aboutmovies:user', JSON.stringify(user))
+
+      setData({ user, token: data.token})
+      
+      alert('Perfil atualizado')
+    } catch(error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('Não foi possível atualizar o perfil')
+      }
+    }
+  }
+
   useEffect(() => {
     const user = localStorage.getItem('@aboutmovies:user')
     const token = localStorage.getItem('@aboutmovies:token')
@@ -44,7 +63,7 @@ function AuthProvider({ children }) {
   }, [])
 
   return (
-    <AuthContext.Provider value={{signIn, signOut, user: data.user}}>
+    <AuthContext.Provider value={{signIn, signOut, updateProfile, user: data.user}}>
       {children}
     </AuthContext.Provider>
   )
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -6,12 +6,46 @@ import { BackButton } from '../../components/BackButton'
 
 import {FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
+import { useState } from 'react'
+import { useAuth } from '../../hooks/auth'
+import { useNavigate } from 'react-router-dom'
+
 
 export function Profile() {
+    const navigate = useNavigate()
+    const { user, updateProfile } = useAuth()
+
+    const [name, setName] = useState(user.name)
+    const [email, setEmail] = useState(user.email)
+
+    const [oldPassword, setOldPassword] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+
+    function handleBack() {
+        navigate(-1)
+    }
+
+    async function handleUpdateProfile() {
+        const data = {
+            name,
+            email,
+            old_password: oldPassword,
+            password: newPassword
+        }
+
+        const userUpdate = Object.assign(user, data)
+        console.log(user)
+        console.log(data)
+        console.log(userUpdate)
+  
+
+        await updateProfile({ user: userUpdate })
+    }
+
     return (
         <Container>
             <header>
-                <BackButton to="/" icon={FiArrowLeft} title="Voltar" />
+                <BackButton onClick={handleBack} icon={FiArrowLeft} title="Voltar" />
             </header>
 
             <Form>
@@ -32,23 +66,33 @@ export function Profile() {
 
                 <Input 
                     icon={FiUser} 
-                    placeholder="Nome" 
+                    placeholder="Nome"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
                 />
                 <Input 
                     icon={FiMail} 
                     placeholder="E-mail"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
                 />
 
                 <Input 
                     icon={FiLock} 
                     placeholder="Senha atual"
+                    type="password"
+                    value={oldPassword}
+                    onChange={event => setOldPassword(event.target.value)}
                 />
                 <Input 
                     icon={FiLock} 
                     placeholder="Senha nova"
+                    type="password"
+                    value={newPassword}
+                    onChange={event => setNewPassword(event.target.value)}
                 />
 
-                <Button title="Salvar"/>
+                <Button title="Salvar" onClick={handleUpdateProfile}/>
             </Form>
         </Container>
     )
