84feed8d88af1bb0d0ee85b6d5c4339eadbf18aa
Murilo Damario <murilo.gcdamario@gmail.com>
2024-08-05 22:48:21 -0300

    Implement renderStar function on Note component

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -6,15 +6,24 @@ import { TfiStar } from 'react-icons/tfi'
 import { Tag } from '../Tag'
 
 export function Note({ data, ...rest}) {
+    
+    function renderStars(rating) {
+        const stars = [];
+        for (let i = 0; i < 5; i++) {
+            if (i < rating) {
+                stars.push(<IoStarSharp key={i} />);
+            } else {
+                stars.push(<TfiStar key={i} />);
+            }
+        }
+        return stars;
+    }
+    
     return (
         <Container {...rest}>
             <h2>{data.title}</h2>
             
-            <IoStarSharp />
-            <IoStarSharp />
-            <IoStarSharp />
-            <IoStarSharp />
-            <TfiStar />
+            {renderStars(data.rating)}
 
             <p>
                 {data.description}
